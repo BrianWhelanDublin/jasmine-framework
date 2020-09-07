e15eef216f7a8c5bcf91a3945930e6847be6edaa
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2020-09-07 09:54:16 +0000

    changed function and tests

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,22 @@
-const addition = () => {
-    return 42;
+Calculator = function() {
+    this.value = 0;
 }
+Calculator.prototype.add = function(num){
+    if(typeof(num) == "number"){
+        this.value += num;
+    } else {
+        alert("Error!")
+    }
+}
+
+
+
+/*
+const addition = (a, b) => {
+  if (typeof a == "number" && typeof b == "number") {
+    return a + b;
+  } else {
+    alert("Error!");
+  }
+};
+*/
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,41 @@
-describe("Calculator", ()=> {
-    describe("Addition tests", ()=> {
-        it("should return 42", ()=> {
-            expect(addition(20, 22)).toBe(42);
-        });
+describe("Calculator", () => {
+beforeEach(function(){
+    calc= new Calculator;
+})
+let calc = new Calculator;
+  describe("Addition tests", () => {
+    it("should return 42", () => {
+        calc.add(20);
+        calc.add(22);
+      expect(calc.value).toBe(42);
     });
+    it("should return 26", () => {
+        calc.add(7);
+        calc.add(19);
+      expect(calc.value).toBe(26);
+    });
+    it("should return an error if we dont't supply two numbers", () => {
+      spyOn(window, "alert");
+      calc.add("Hitchhikers");
+      expect(window.alert).toHaveBeenCalledWith("Error!")
+    });
+  });
 });
+
+/*
+describe("Calculator", () => {
+  describe("Addition tests", () => {
+    it("should return 42", () => {
+      expect(addition(20, 22)).toBe(42);
+    });
+    it("should return 26", () => {
+      expect(addition(7, 19)).toBe(26);
+    });
+    it("should return an error if we dont't supply two numbers", () => {
+      spyOn(window, "alert");
+      addition("Hitchhikers", "Guide");
+      expect(window.alert).toHaveBeenCalledWith("Error!")
+    });
+  });
+});
+*/
